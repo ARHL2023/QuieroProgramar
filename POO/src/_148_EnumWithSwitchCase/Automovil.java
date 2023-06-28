931f39ebc5dde702966d7fb716a11a9a31bcf5aa
ARHL2023 <129900679+ARHL2023@users.noreply.github.com>
2023-06-28 10:33:55 -0500

    148 ENUM con SWITCH CASE

---
+++ POO/src/_148_EnumWithSwitchCase/Automovil.java
@@ -0,0 +1,66 @@
+package _148_EnumWithSwitchCase;
+
+public class Automovil {
+
+    private String fabricante;
+    private String modelo;
+    private Color color; //Color es de la clase Enum
+
+    private TipoAutomovil tipo;
+
+    public Automovil(){
+
+    }
+
+    public Automovil(String fabricante, String modelo, Color color) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
+    public TipoAutomovil getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoAutomovil tipo) {
+        this.tipo = tipo;
+    }
+
+    public String detalle() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nauto.fabricante = " + this.fabricante);
+        sb.append("\nauto.modelo = " + this.modelo);
+        sb.append("\nauto.color = " + this.color.getColor());//como estaba asiciona el getColor paar hacer uso del contenido de minusculas
+        sb.append("\nauto.tipo = " + this.getTipo().getNombre());
+        return sb.toString();
+    }
+
+}//CLASS
+
+
---
+++ POO/src/_148_EnumWithSwitchCase/Principal.java
@@ -0,0 +1,42 @@
+package _148_EnumWithSwitchCase;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Automovil auto1 = new Automovil();
+
+        auto1.setModelo("Modelo 1");
+        auto1.setFabricante("Fabricaente 1");
+        auto1.setColor(Color.AZUL);
+        auto1.setTipo(TipoAutomovil.CONVERTIBLE);
+
+        Automovil auto2 = new Automovil();
+
+        auto2.setModelo("Modelo 2");
+        auto2.setFabricante("Fabricaente 2");
+        auto2.setColor(Color.AMARILLO);
+        auto2.setTipo(TipoAutomovil.SEDAN);
+
+        System.out.println(auto1.detalle());
+        System.out.println(auto2.detalle());
+
+        /////////////////////////////////////////
+        TipoAutomovil tipoAuto1 = auto1.getTipo();
+        System.out.println(auto1.getFabricante());
+
+        switch (tipoAuto1){
+            case SEDAN :
+                System.out.println("ES UN CASO SEDAN");
+                break;
+            case FURGON:
+                System.out.println("ES UN CASO FURGON");
+                break;
+            case CONVERTIBLE:
+                System.out.println("ES UN CASO CONVERTIBLE");
+                break;
+        }//SWITCH
+
+    }//MAIN
+}//CLASS
