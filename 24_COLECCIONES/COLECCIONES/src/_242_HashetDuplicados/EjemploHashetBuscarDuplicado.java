1e26a8bd4c0b23d8a9b21d1eb4ceba2ddd192e44
ARHL2023 <129900679+ARHL2023@users.noreply.github.com>
2023-07-03 14:09:04 -0500

    242

---
+++ 24_COLECCIONES/COLECCIONES/src/_242_HashetDuplicados/EjemploHashetBuscarDuplicado.java
@@ -0,0 +1,9 @@
+package _242_HashetDuplicados;
+
+public class EjemploHashetBuscarDuplicado {
+    public static void main(String[] args) {
+
+        String peces[] = {"Corvina", "Lenguado", "Pejerrey", "Robalo", "Atun", "Lenguado"};
+
+    }
+}
